d5505db0fdd25dcd97dbdc01deb0242951cc4ca0
Pippert <63615852+Pippert@users.noreply.github.com>
2020-04-20 16:35:09 -0500

    Add files via upload

--- Chemistry/Chemistry.py
+++ Chemistry/Chemistry.py
@@ -7,34 +7,43 @@ This is a temporary script file.
 import math
 
 def primary_density(T):
-    " This function returns density of the fuel mixture in g/cm^3, T must be in celsius "
+    """ This function returns density of the fuel 
+    mixture in g/cm^3, T must be in celsius """
+    
     density = 2.38 - (40E-5) * T
     return density
 
 def primary_viscosity(T):
-    " This function returns viscosity of the fuel mixture in cP, T must be in celsius "
+    """ This function returns viscosity of the 
+    fuel mixture in cP, T must be in celsius """
+    
     viscosity = 8.4 
     return viscosity
 
 def secondary_density(T): 
-    " This function returns density of the coolant mixture in g/cm^3, T must be in celsius "
+    """ This function returns density of the coolant 
+    mixture in g/cm^3, T must be in celsius """
+    
     density = 2.16 - (40E-5) * T
     return density
 
 def secondary_viscosity(T):
-    " This function returns viscosity of the coolant mixture in cP, T must be in celsius "
+    """ This function returns viscosity of the coolant 
+    mixture in cP, T must be in celsius """
+    
     T= T+273
     viscosity = 0.118 * math.exp(3624 / T) 
     return viscosity
 
 
 def solubilitypercentage(T):
-    " This function takes a temperature and 
-    " returns the solubility of the fuel mixture in decimal form "
-    " x = BeF2
-    " y = ZrF4
-    " z = UF4
-    " T must be in celcius 
+    """ This function takes a temperature and 
+    returns the solubility of the fuel mixture 
+    in decimal form 
+    x = BeF2
+    y = ZrF4
+    z = UF4
+    T must be in celcius """
     
     def solubility(w,x,y,z):
         solubility = 0.65 * w + 0.29 * x + 0.05 * y + 0.01 * z
