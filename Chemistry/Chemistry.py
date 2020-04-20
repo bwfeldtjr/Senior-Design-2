6818f802733ab2b7d929d763e0b073d628c3d60a
Pippert <63615852+Pippert@users.noreply.github.com>
2020-04-20 16:31:16 -0500

    Add files via upload

--- Chemistry/Chemistry.py
+++ Chemistry/Chemistry.py
@@ -6,22 +6,63 @@ This is a temporary script file.
 """
 import math
 
-def primary_density(T): # T must be in celcius
-    density = 2.38 - (40E-5) * T # [g/cm^3]
+def primary_density(T):
+    " This function returns density of the fuel mixture in g/cm^3, T must be in celsius "
+    density = 2.38 - (40E-5) * T
     return density
 
 def primary_viscosity(T):
-    viscosity = 8.4 # [cP] At 600 celcius
+    " This function returns viscosity of the fuel mixture in cP, T must be in celsius "
+    viscosity = 8.4 
     return viscosity
 
-def secondary_density(T): # T must be in celcius
-    density = 2.16 - (40E-5) * T # [g/cm^3]
+def secondary_density(T): 
+    " This function returns density of the coolant mixture in g/cm^3, T must be in celsius "
+    density = 2.16 - (40E-5) * T
     return density
 
-def secondary_viscosity(T): # T must be in celsius
+def secondary_viscosity(T):
+    " This function returns viscosity of the coolant mixture in cP, T must be in celsius "
     T= T+273
     viscosity = 0.118 * math.exp(3624 / T) # [cP]
     return viscosity
 
+#w = liF
+#x = BeF2
+#y = ZrF4
+#z = UF4
+def solubilitypercentage(T):
+    " This function takes a temperature and 
+    " returns the solubility of the fuel mixture in decimal form "
+    " x = BeF2
+    " y = ZrF4
+    " z = UF4
+    " T must be in celcius 
+    
+    def solubility(w,x,y,z):
+        solubility = 0.65 * w + 0.29 * x + 0.05 * y + 0.01 * z
+        return solubility
+    
+    if T < 458:
+        sol = solubility(0, 0, 0, 0)
+        print sol
+    
+    elif T < 560:
+        sol = solubility(0, 1, 0, 0)  
+        print sol
+    
+    elif T < 910:
+        sol = solubility(1, 1, 0, 0)
+        print sol
+        
+    elif T < 1035:
+        sol = solubility(1, 1, 1, 0)
+        print sol
+    
+    else:
+        sol = solubility(1, 1, 1, 1)
+        print sol
+    return sol
+    
     
     
