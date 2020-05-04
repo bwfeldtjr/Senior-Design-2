6d75c5ed9cd895e3ca0fdfc03219a460c0927a69
Bjorn412 <63613613+Bjorn412@users.noreply.github.com>
2020-05-04 16:53:43 -0500

    Add files via upload

--- Fluids/Fluids.py
+++ Fluids/Fluids.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-author: Bjorn Funk
+Created on Mon Apr 20 14:42:40 2020
+
+@author: Bjorn
 """
 
 import math
 
 from reynolds import Reynoldsnum
-from major_head_loss import HlMajor
-from minor_head_loss import HlMinor
-import Chemistry
+from majorheadloss import HlMajor
+from minorheadloss import HlMinor
+import chemistry as Chemistry
 
 
 def function(fluidTempReactor, fluidTempHeatEx):
@@ -18,35 +20,33 @@ def function(fluidTempReactor, fluidTempHeatEx):
     #Constants
     g = 9.807 #m/s^2
     
-    #FINISH UP FINAL SIZE and FITTING DESIGN FOR THIS
     #Design of MSR
     #Assumed diameter of all connecting pipes (From MSRE)
     diameter = 0.1524 #m
     #Assumed volumetric flow rate (From MSRE)
-    volFlowRate =  0.00336 #m^3/s
+    volFlowRate =  0.07571 #m^3/s
     fluidVelocity = volFlowRate/((diameter/2)**2*math.pi)
 
-    fuelLoopDesign = [[0, 1],#1 First digit is height coordinate, second is length of pipe to next
-                      [1, 0],#2
-                      [1, 100],#3
-                      [1, 0],#4
-                      [1, 4],#5
-                      [-3, 0],#6
-                      [-3, 1],#7
-                      [-2, 1]]#8
+    fuelLoopDesign = [[0.0, 3.2],#1 First digit is height coordinate, second is length of pipe to next
+                      [2.2, 0.0],#2
+                      [2.2, 0.762],#3
+                      [2.2, 0],#4
+                      [1.7, 0.1],#5
+                      [1.6, 0],#6
+                      [0.1, 2.362],#7
+                      [0.0, 0]]#8
     
-    #FINISH UP WHAT FITTINGS WILL BE IN THIS DESIGN, ADD T FITTINGS FOR FREEZEPLUG
     #array of fittings from that point to next
     fuelLoopFittings =[['90deg elbow, standard r'],#1
                        ['none'],#2
                        ['90deg elbow, standard r'],#3
                        ['none'],#4
-                       ['90deg elbow, long r'],#5
+                       ['none'],#5
                        ['none'],#6
-                       ['none'],#7
+                       ['90deg elbow, standard r','90deg elbow, standard r'],#7
                        ['none']]#8
         
-    #Fluid properties as a function of temperature
+    #Fluid properties as a function of temperature after the reactor(R) and heat exchanger (HE)
     densityR = Chemistry.primary_density(fluidTempReactor)*1000#kg/m^3
     densityHE = Chemistry.primary_density(fluidTempHeatEx)*1000#kg/m^3
     viscosityR = Chemistry.primary_viscosity(fluidTempReactor)/1000#Pa*s
@@ -54,13 +54,19 @@ def function(fluidTempReactor, fluidTempHeatEx):
     ReynoldsR = Reynoldsnum(densityR, volFlowRate, diameter, viscosityR)
     ReynoldsHE = Reynoldsnum(densityHE,volFlowRate,diameter,viscosityHE)
  
-    #GET BETTER ESTIMATES OF THESE HEADS
     #Head loss
     #head of components in fuel loop
-    pumpHead = 14.33#m
-    heatExchangerHead = -3.0#m
-    purificationSystemHead = -1.0#m
-    reactorHead = -3.0#m
+    pumpHead = 0#m  Initial number
+    heatExchangerHead = -7.74#m
+    #Purification System
+    purificationDiameter = 0.9144#m
+    purificationLength = 1.524#m
+    purificationVel = volFlowRate/(math.pi*(purificationDiameter/2)**2)#m/s
+    purEntranceHead = (1.1*(fluidVelocity-purificationVel)**1.92)/(2*g)#m
+    purExitHead = (1/(math.pi*(purificationDiameter/2)**2/(math.pi*(diameter/2)**2)))
+    ReynoldsPur = Reynoldsnum(densityHE,volFlowRate,purificationDiameter,viscosityHE)
+    purificationSystemHead = -(purEntranceHead + purExitHead + HlMajor(purificationLength,purificationVel,purificationDiameter,ReynoldsPur))#m
+    reactorHead = -0.3048*10**(-5)*(volFlowRate*15850.323)**2.01#m
     #First digit is major head loss from that point to next, second digit is minor head loss from that point to next,third is other head loss from that point to next 
     head = [[HlMajor(fuelLoopDesign[0][1],fluidVelocity,diameter,ReynoldsR), HlMinor(fluidVelocity,fuelLoopFittings[0]), 0],#1
             [0, 0, pumpHead],#2
@@ -70,10 +76,15 @@ def function(fluidTempReactor, fluidTempHeatEx):
             [0, 0, purificationSystemHead],#6
             [HlMajor(fuelLoopDesign[6][1],fluidVelocity,diameter,ReynoldsHE), HlMinor(fluidVelocity, fuelLoopFittings[6]), 0],#7
             [0, 0, reactorHead]]#8
-    
+
+    #Determine pump head requirements
+    for i in range(len(head)):
+        for j in range(3):
+            pumpHead += head[i][j]            
+    head[1][2] = -pumpHead
+
     #Pressure array
     pressure = []
-    #FIND BETTER INITIAL PRESSURE
     #initial pressure exiting the reactor core
     pressure.append(0.0)#pa
     #pressure array first cycles through items before the heat exchanger then after heat exchanger
@@ -81,18 +92,88 @@ def function(fluidTempReactor, fluidTempHeatEx):
         if i<3:
             pressure.append(pressure[i-1]+densityR*g*(fuelLoopDesign[i][0]-fuelLoopDesign[i-1][0] + head[i][0] + head[i][1] + head[i][2]))
         else:
-            pressure.append(pressure[i-1]+densityHE*g*(fuelLoopDesign[i][0]-fuelLoopDesign[i-1][0] + head[i][0] + head[i][1] + head[i][2]))
+            pressure.append(pressure[i-1]+densityR*g*(fuelLoopDesign[i][0]-fuelLoopDesign[i-1][0] + head[i][0] + head[i][1] + head[i][2]))
+
+    #Compute mass flow rate for after heat exchanger and after reactor 
+    massFlowRateR = volFlowRate*densityR #After Reactor
+    massFlowRateHE = volFlowRate*densityHE #After Heat Exchanger
+    
+    
+    
+    return [pressure, massFlowRateR, massFlowRateHE]
+
+def coolant(fluidTempHot, fluidTempCold):
+    '''
+    Returns pressure and mass flow rates of the coolant loop using the hot and cold fluid temperatures
+    '''
+     #Constants
+    g = 9.807 #m/s^2
+    
+    #Design of MSR
+    #Assumed diameter of all connecting pipes (From MSRE)
+    diameter = 0.1524 #m
+    #Assumed volumetric flow rate (From MSRE)
+    volFlowRate =  0.05363 #m^3/s
+    fluidVelocity = volFlowRate/((diameter/2)**2*math.pi)
+
+    fuelLoopDesign = [[0.0, 3.2],#1 First digit is height coordinate, second is length of pipe to next
+                      [2.2, 0.0],#2
+                      [2.2, 0.762],#3
+                      [2.2, 0],#4
+                      [1.7, 0.1],#5
+                      [1.6, 0]]#6
+
+    
+    #array of fittings from that point to next
+    fuelLoopFittings =[['90deg elbow, standard r'],#1
+                       ['none'],#2
+                       ['90deg elbow, standard r'],#3
+                       ['none'],#4
+                       ['none'],#5
+                       ['none']]#6
+        
+    #Fluid properties as a function of temperature after the fuel/coolant (HE1) heat exchanger and the 
+    densityHE1 = Chemistry.secondary_density(fluidTempHot)*1000#kg/m^3
+    densityHE2 = Chemistry.secondary_density(fluidTempCold)*1000#kg/m^3
+    viscosityHE1 = Chemistry.secondary_viscosity(fluidTempHot)/1000#Pa*s
+    viscosityHE2 = Chemistry.secondary_viscosity(fluidTempCold)/1000#Pa*s
+    ReynoldsHE1 = Reynoldsnum(densityHE1, volFlowRate, diameter, viscosityHE1)
+    ReynoldsHE2 = Reynoldsnum(densityHE2,volFlowRate,diameter,viscosityHE2)
+ 
+    #Head loss
+    #head of components in coolant loop
+    pumpHead = 0#m
+    #Fuel/coolant heat exchanger
+    heatExchanger1Head = -3.0#m
+    #Heat exchanger for thermal power
+    heatExchanger2Head = -3.0#m
+    #First digit is major head loss from that point to next, second digit is minor head loss from that point to next,third is other head loss from that point to next 
+    head = [[HlMajor(fuelLoopDesign[0][1],fluidVelocity,diameter,ReynoldsHE1), HlMinor(fluidVelocity,fuelLoopFittings[0]), 0],#1
+            [0, 0, pumpHead],#2
+            [HlMajor(fuelLoopDesign[2][1],fluidVelocity,diameter,ReynoldsHE1), HlMinor(fluidVelocity,fuelLoopFittings[2]), 0],#3
+            [0, 0, heatExchanger2Head],#4
+            [HlMajor(fuelLoopDesign[4][1],fluidVelocity,diameter,ReynoldsHE2), HlMinor(fluidVelocity, fuelLoopFittings[4]), 0],#5
+            [0, 0, heatExchanger1Head]]#6
+    
+    #Determine pump head requirements
+    for i in range(len(head)):
+        for j in range(3):
+            pumpHead += head[i][j]            
+    head[1][2] = -pumpHead
+
+    #Pressure array
+    pressure = []
+    #initial pressure exiting the reactor core
+    pressure.append(0.0)#pa
+    #pressure array first cycles through items before the heat exchanger then after heat exchanger
+    for i in range(1,6):
+        if i<3:
+            pressure.append(pressure[i-1]+densityHE1*g*(fuelLoopDesign[i][0]-fuelLoopDesign[i-1][0] + head[i][0] + head[i][1] + head[i][2]))
+        else:
+            pressure.append(pressure[i-1]+densityHE2*g*(fuelLoopDesign[i][0]-fuelLoopDesign[i-1][0] + head[i][0] + head[i][1] + head[i][2]))
     
     #Comput mass flow rate for 
-    massFlowRateR = volFlowRate*densityR
-    massFlowRateHE = volFlowRate*densityHE
+    massFlowRateHE1 = volFlowRate*densityHE1 #After Heat Exchanger 1
+    massFlowRateHE2 = volFlowRate*densityHE2 #After Heat Exchanger 2
     
-    return pressure, massFlowRateR, massFlowRateHE
-        
-            
-        
-        
-        
-        
-        
-        
+    return [pressure, massFlowRateHE1, massFlowRateHE2]
